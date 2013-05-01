3d0733853a32f6a1135953b5972aa76e9a634fee
Jonathan Fischoff <jonathangfischoff@gmail.com>
2013-04-30 20:33:02 -0700

    added executable to the cabal file

---
+++ src/Example.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE RecordWildCards #-}
+module Main where
+
+import qualified Graphics.Obedient.Internal     as OBD
+import           Graphics.Obedient.Internal.SDL
+
+  -- This imports all the mouse events/behaviors at the top level.
+OBD.Inputs {..} = initializeWindow 400 400
+
+main = runApp (fmap show leftClick)
